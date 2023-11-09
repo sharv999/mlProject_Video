433893831bb02496c48a10909deaca1b81f90deb
sharv999 <steve@healthphysics.com.au>
2023-11-10 01:14:55 +1100

    First commit

--- sketch.js
+++ sketch.js
@@ -1,33 +1,45 @@
-// Globals xxxx
+// Ref: 
+//https://www.youtube.com/watch?v=D9BoBSkLvFo
+
+// Server
+// $ http-server -c-1 -a localhost -p 8000
+
+// Server not refreshing, camera not working
+
+// Globals
 let classifier;
-let img;
+let video;
 
 // ml5 preload function
 function preload() {
-  classifier = ml5.imageClassifier('MobileNet', modelReady);
+  classifier = ml5.imageClassifier('MobileNet', video, modelReady);
 }
 
 // ml5 setup function
 function setup() {
   createCanvas(400, 400);
-  img = createImg('images/bird.jpg', imageReady);
-  img.hide();
+  video = createCapture(VIDEO);
+  video.hide();
   background(0);
+}
+
+function draw() {
+  image(video, 0, 0);
 }
 
 // callback function for when the model is ready
 function modelReady() {
 	console.log('Model ready');
-	classifier.classify(img, gotResult);
+  classifier.predict(gotResults);
 }
 
 // callback function for when the image is ready
-function imageReady() {
+////function imageReady() {
 	image(img, 0, 0, width, height);
-}
+//}
 
 // A callback function to run when we get any errors and the results
-function gotResult(error, results) {
+function gotResults(error, results) {
   // Display error in the console
   if (error) {
     console.error(error);
