a838361aa715c3ce1bffc50615cae0067346b315
sharv999 <steve@healthphysics.com.au>
2023-11-11 00:07:07 +1100

    Video working

--- sketch.js
+++ sketch.js
@@ -1,30 +1,35 @@
-// Ref: 
-//https://www.youtube.com/watch?v=D9BoBSkLvFo
+// Ref: https://www.youtube.com/watch?v=D9BoBSkLvFo
 
 // Server
 // $ http-server -c-1 -a localhost -p 8000
 
-// Server not refreshing, camera not working
+// Camera not working (permissions?)
 
 // Globals
 let classifier;
-let video;
+let capture;
+let label = "";
 
-// ml5 preload function
-function preload() {
-  classifier = ml5.imageClassifier('MobileNet', video, modelReady);
-}
+// preload function
+//function preload() {
+//  classifier = ml5.imageClassifier('MobileNet', capture, modelReady);
+//}
 
-// ml5 setup function
+// setup function
 function setup() {
   createCanvas(400, 400);
-  video = createCapture(VIDEO);
-  video.hide();
+  capture = createCapture(VIDEO);
+  capture.hide();
   background(0);
+  classifier = ml5.imageClassifier('MobileNet', capture, modelReady);
 }
 
 function draw() {
-  image(video, 0, 0);
+  // Draw webcam image onto canvas
+  image(capture, 0, 0);
+  fill(0);
+  textSize(32);
+  text(label, 10, height - 10);
 }
 
 // callback function for when the model is ready
@@ -35,7 +40,7 @@ function modelReady() {
 
 // callback function for when the image is ready
 ////function imageReady() {
-	image(img, 0, 0, width, height);
+	//image(img, 0, 0, width, height);
 //}
 
 // A callback function to run when we get any errors and the results
@@ -44,9 +49,11 @@ function gotResults(error, results) {
   if (error) {
     console.error(error);
   } else {
-    // The results are in an array ordered by confidence.
+    // The results are in an array ordered by confidence
+    classifier.predict(gotResults);
     console.log(results);
-    createDiv(`Label: ${results[0].label}`);
-    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+    label = results[0].label;
+    //createDiv(`Label: ${results[0].label}`);
+    //createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
   }
 }
